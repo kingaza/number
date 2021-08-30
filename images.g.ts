00637eda994f69e827351de84a0f953b1a8d1c24
kingaza <kingaza54@outlook.com>
2021-08-31 02:29:19 +0800

    MakeCode 项目的初始文件

---
+++ images.g.ts
@@ -0,0 +1,208 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "num_2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f . . . f f f . . . 
+. . . f f . . . . . f f f . . . 
+. . . f . . . . . . . f f . . . 
+. . . . . . . . . . . f f . . . 
+. . . . . . . . . . f f f . . . 
+. . . . . . . . f f f f . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . f f f f . . . . . . . . 
+. . . f f f . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "num_3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f f f f f f f f f . . . . 
+. . . f f . . . . f f f . . . . 
+. . . f f . . . . . f f . . . . 
+. . . . . . . . . f f f . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . . . . . f f f . . . . 
+. . . f f . . . . . f f . . . . 
+. . . f f . . . . f f f . . . . 
+. . . f f f f f f f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "num_1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . f f f f . . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "num_4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f f f . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . f f . f f . . . . . 
+. . . . . f f . . f f . . . . . 
+. . . . . f f . . f f . . . . . 
+. . . . f f . . . f f . . . . . 
+. . . f f f . . . f f . . . . . 
+. . . f f . . . . f f . . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . f f . . . . . 
+. . . . . . . . . f f . . . . . 
+. . . . . . . . . f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "num_5":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . f f . . . . . . . . . . 
+. . . . f f . . . . . . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f f . . . 
+. . . . f f . . . . . f f f . . 
+. . . . . . . . . . . . f f . . 
+. . . . . . . . . . . . f f . . 
+. . . . f f . . . . . . f f . . 
+. . . . f f f . . . . f f f . . 
+. . . . . f f f f f f f f . . . 
+. . . . . . . f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "num_6":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f f f . . . 
+. . . . . f f f f f f f f . . . 
+. . . . f f f . . . . f f . . . 
+. . . . f f . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . . f f f f f f f f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "num_7":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f f f . . 
+. . . . f f f f f f f f f f . . 
+. . . . . . . . . . . . f f . . 
+. . . . . . . . . . . f f f . . 
+. . . . . . . . . . . f f . . . 
+. . . . . . . . . . f f . . . . 
+. . . . . . . . . f f f . . . . 
+. . . . . . . . . f f . . . . . 
+. . . . . . . . f f . . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image8":
+            case "num_8":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f . . . . f f . . . . 
+. . . . f f . . . . f f . . . . 
+. . . . f f f . . f f f . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f . . . . . . f f . . . 
+. . . f f . . . . . . f f . . . 
+. . . f f . . . . . . f f . . . 
+. . . f f . . . . . . f f . . . 
+. . . . f f f f f f f f . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image9":
+            case "num_9":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . f f f f f f f f . . . 
+. . . . f f . . . . . f f f . . 
+. . . . f f . . . . . . f f . . 
+. . . . f f . . . . . . f f . . 
+. . . . f f . . . . . f f f . . 
+. . . . . f f f f f f f f f . . 
+. . . . . . . . . . . . f f . . 
+. . . . . . . . . . . . f f . . 
+. . . . . . . . . . . . f f . . 
+. . . . f f . . . . . f f f . . 
+. . . . f f f f f f f f f . . . 
+. . . . . f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image10":
+            case "num_0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . f f f . f f f . . . . 
+. . . . f f f . . . f f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f . . . . . f f . . . 
+. . . . f f f . . . f f f . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
---
+++ main.ts
@@ -0,0 +1,2 @@
+let tens_digit = randint(1, 9)
+let unit_digit = randint(0, 9)
