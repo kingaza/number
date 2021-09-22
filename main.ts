5e530fb7879637f555559fae28c3bc5c01eaf5b5
kingaza <kingaza54@outlook.com>
2021-09-23 00:41:50 +0800

    add 'one more game' after winning

--- main.ts
+++ main.ts
@@ -1,43 +1,47 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     num_viewer.count += 10
-    num_pred += 10
+    num_guess += 10
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     num_viewer.count += -10
-    num_pred += -10
+    num_guess += -10
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (num_pred > num_true) {
+    if (num_guess > num_answer) {
         num_viewer.setDigitColor(2)
         music.pewPew.play()
         info.changeLifeBy(-1)
-    } else if (num_pred < num_true) {
-        num_viewer.setDigitColor(10)
+    } else if (num_guess < num_answer) {
+        num_viewer.setDigitColor(8)
         music.pewPew.play()
         info.changeLifeBy(-1)
     } else {
         num_viewer.setDigitColor(7)
         music.baDing.play()
+        game.splash("再来一局？")
+        if (controller.A.isPressed()) {
+            game.reset()
+        }
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     num_viewer.count += 1
-    num_pred += 1
+    num_guess += 1
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    game.splash(num_true)
+    game.splash(num_answer)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     num_viewer.count += -1
-    num_pred += -1
+    num_guess += -1
 })
 let num_viewer: DigitCounter = null
-let num_pred = 0
-let num_true = 0
+let num_guess = 0
+let num_answer = 0
 info.setLife(8)
 let num_init = 55
-num_true = randint(10, 99)
-num_pred = num_init
+num_answer = randint(10, 99)
+num_guess = num_init
 num_viewer = sevenseg.createCounter(SegmentStyle.Thick, SegmentScale.Full, 2)
 num_viewer.count = num_init
 num_viewer.setDigitColor(5)
